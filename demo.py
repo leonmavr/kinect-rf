f2d113ae55ba0a5b6a166baf8ba9981766ea45c0
leonmavr <0xleo.git@gmail.com>
2024-11-01 10:00:51 +0100

    Add bounding boxes to demo

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ import pickle
 cap = cv2.VideoCapture(0)
 
 if __name__ == '__main__':
-    clf_path = os.path.join('.', 'rf_head_hands.clf')
+    clf_path = os.path.join('.', 'rf_head_hands_01_32_absdiff.clf')
     with open(clf_path, 'rb') as f:
         clf = pickle.load(f)
 
@@ -18,9 +18,11 @@ if __name__ == '__main__':
     while True:
         valid, img_depth = cap.read()
         img_depth = cv2.cvtColor(img_depth, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('pre-recorded video', img_depth)
+        cv2.imshow('video', img_depth)
         img_predicted = predict(img_depth, clf)
+        boxes = find_bboxes(img_predicted)
+        img_predicted = draw_bboxes(img_depth, boxes)
         cv2.imshow('predicted labels', img_predicted)
-        c = cv2.waitKey(50)
+        c = cv2.waitKey(33)
         if c == ord('q') or not valid:
             break
